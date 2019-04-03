06160489b387120e368c0732fc5351927f1040fd
Alex McChesney <alex.mcchesney@gmail.com>
2019-04-03 08:44:25 -0500

    Stripped down tinyfont to save program space

--- Font3x5.cpp
+++ Font3x5.cpp
@@ -6,15 +6,17 @@
 #define FONT3X5_WIDTH 3
 #define FONT3X5_HEIGHT 6
 
+/*
 #define CHAR_EXCLAMATION 33
 #define CHAR_PERIOD 46
 #define CHAR_LETTER_A 65
 #define CHAR_LETTER_Z 90
 #define CHAR_LETTER_A_LOWER 97
-#define CHAR_LETTER_Z_LOWER 122
+#define CHAR_LETTER_Z_LOWER 122*/
 #define CHAR_NUMBER_0 48
 #define CHAR_NUMBER_9 57
 
+/*
 #ifdef USE_LOWER_CASE
 #define FONT_EXCLAMATION_INDEX 62
 #define FONT_PERIOD_INDEX 63
@@ -22,12 +24,14 @@
 #else
 #define FONT_EXCLAMATION_INDEX 36
 #define FONT_PERIOD_INDEX 37
-#define FONT_NUMBER_INDEX 26
-#endif
+*/
+#define FONT_NUMBER_INDEX 0 /*26*/
+/*
+#endif*/
 
 const uint8_t PROGMEM font_images[] = {
 	3, 8,
-	0x1F, 0x05, 0x1F, // A
+	/*0x1F, 0x05, 0x1F, // A
 	0x1F, 0x15, 0x1B,
 	0x1F, 0x11, 0x11,
 	0x1F, 0x11, 0x0E,
@@ -80,7 +84,7 @@ const uint8_t PROGMEM font_images[] = {
 	0x1A,	0x04,	0x1A,
 	0x2E,	0x28,	0x1E,
 	0x1A,	0x12,	0x16, // z
-#endif
+#endif*/
 	0x1F, 0x11, 0x1F, // 0  
 	0x00, 0x1F, 0x00,
 	0x1D, 0x15, 0x17,
@@ -91,28 +95,31 @@ const uint8_t PROGMEM font_images[] = {
 	0x01, 0x01, 0x1F,
 	0x1F, 0x15, 0x1F,
 	0x17, 0x15, 0x1F, // 9
-	0x00, 0x17, 0x00, // !
-	0X00, 0x10, 0x00, // .
+	/*0x00, 0x17, 0x00, // !
+	0X00, 0x10, 0x00, // . */
 };
 
+#define LINE_SPACING	8
+#define LETTER_SPACING	1
+#define TEXT_COLOR		1
 
-Font3x5::Font3x5(uint8_t lineSpacing) {
+Font3x5::Font3x5(/*uint8_t lineSpacing*/) {
 
-	_lineHeight = lineSpacing;
-	_letterSpacing = 1;
+	/*_lineHeight = lineSpacing;
+	_letterSpacing = 1;*/
 
 	_cursorX = _cursorY = _baseX = 0;
-	_textColor = 1;
+	/*_textColor = 1;*/
 
 }
 
 size_t Font3x5::write(uint8_t c) {
 
-	if (c == '\n') { _cursorX = _baseX; _cursorY += _lineHeight; }
+	if (c == '\n') { _cursorX = _baseX; _cursorY += /*_lineHeight*/ LINE_SPACING; }
 	else {
 
 		printChar(c, _cursorX, _cursorY);
-		_cursorX += FONT3X5_WIDTH + _letterSpacing;
+		_cursorX += FONT3X5_WIDTH + LETTER_SPACING;
 
 	}
 
@@ -127,7 +134,7 @@ void Font3x5::printChar(const char c, const int8_t x, int8_t y) {
 	++y;
 
 	switch (c) {
-
+/*
 	case CHAR_LETTER_A ... CHAR_LETTER_Z:
 		idx = c - CHAR_LETTER_A;
 		break;
@@ -137,11 +144,11 @@ void Font3x5::printChar(const char c, const int8_t x, int8_t y) {
 		idx = c - CHAR_LETTER_A_LOWER + 26;
 		break;
 #endif
-
+*/
 	case CHAR_NUMBER_0 ... CHAR_NUMBER_9:
 		idx = c - CHAR_NUMBER_0 + FONT_NUMBER_INDEX;
 		break;
-
+/*
 	case CHAR_EXCLAMATION:
 		idx = FONT_EXCLAMATION_INDEX;
 		break;
@@ -149,15 +156,15 @@ void Font3x5::printChar(const char c, const int8_t x, int8_t y) {
 	case CHAR_PERIOD:
 		idx = FONT_PERIOD_INDEX;
 		break;
-
+		*/
 	}
 
 	if (idx > -1) {
 
-		if (_textColor == WHITE) {
+		//if (_textColor == WHITE) {
 			//Sprites::drawSelfMasked(x, y, font_images, idx);
 			renderSprite(font_images, Vector2Int(x,y), MIRROR_NONE, idx);
-		}
+		//}
 	}
 
 }
@@ -167,6 +174,7 @@ void Font3x5::setCursor(const int8_t x, const int8_t y) {
 	_cursorY = y;
 }
 
+/*
 void Font3x5::setTextColor(const uint8_t color) {
 	_textColor = color;
 }
@@ -174,3 +182,4 @@ void Font3x5::setTextColor(const uint8_t color) {
 void Font3x5::setHeight(const uint8_t color) {
 	_lineHeight = color;
 }
+*/
